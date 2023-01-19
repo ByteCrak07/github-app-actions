469cd10f3f17b1c70c553f59ccc2980671e61054
ByteCrak07 <abilsavio@gmail.com>
2023-01-20 01:46:40 +0530

    Deploy a test workflow file

--- index.js
+++ index.js
@@ -2,26 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
+const fs = require("fs");
+const { App } = require("octokit");
 
 dotenv.config();
 app.use(express.json());
 
-const { App, Octokit } = require("octokit");
-// const {
-//   createOrUpdateTextFile,
-// } = require("@octokit/plugin-create-or-update-text-file");
-
-// const MyOctokit = Octokit.plugin(createOrUpdateTextFile).defaults({
-//   userAgent: "boop-jasons-nose-app/v1.0.0",
-// });
-
-const octokit = new Octokit();
 const githubApp = new App({
   appId: process.env.APP_ID,
   privateKey: process.env.PRIVATE_KEY.replace(/\\n/gm, "\n"),
 });
-
-var repos = {};
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/frontend/index.html"));
@@ -29,37 +19,48 @@ app.get("/", function (req, res) {
 
 app.get("/get-repos", async function (req, res) {
   let repoNames = [];
-  repos = {};
 
-  for await (const {
-    repository,
-    octokit,
-  } of githubApp.eachRepository.iterator()) {
+  for await (const { repository } of githubApp.eachRepository.iterator()) {
     repoNames.push(repository.full_name);
-    repos[repository.full_name] = {
-      owner: repository.owner.login,
-      repo: repository.name,
-      octokit,
-    };
   }
 
-  console.log(repos);
   res.send(repoNames);
 });
 
-app.get("/deploy/:repo", async function (req, res) {
+app.get("/deploy/:user/:repo", async function (req, res) {
+  let user = req.params.user;
   let repo = req.params.repo;
-  let repoOctokit = repos[repo].octokit;
 
-  const { data } = await repoOctokit.rest.repos.getContent({
-    owner: repos[repo].owner,
-    repo: repos[repo].name,
-    path: "hello.js",
-  });
+  try {
+    for await (const {
+      repository,
+      octokit,
+    } of githubApp.eachRepository.iterator()) {
+      if (repository.full_name !== `${user}/${repo}`) continue;
 
-  console.log(data);
+      const createFile = await octokit.request(
+        "PUT /repos/{owner}/{repo}/contents/{path}",
+        {
+          owner: repository.owner.login,
+          repo: repository.name,
+          path: ".github/workflows/test.yml",
+          message: "Add new test workflow",
+          content: fs.readFileSync(
+            path.join(__dirname + "/workflows/test.yml"),
+            {
+              encoding: "base64",
+            }
+          ),
+        }
+      );
 
-  res.send("kiity");
+      console.log(createFile.data);
+      res.send({ message: "Success" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send("error");
+  }
 });
 
 let port = 3000;
